ad0e567c4458c87cff815e86dd0d5e02a8512370
Kenny Wong Chi-Chuen <kennyviperhk@gmail.com>
2018-09-26 18:31:30 +0800

    tcp client teseter update

--- Test-networkTcpClientExample/src/ofApp.cpp
+++ Test-networkTcpClientExample/src/ofApp.cpp
@@ -6,29 +6,38 @@
 void ofApp::setup(){
 	ofSetBackgroundColor(230);
 
+	//======================== Network ==========================
+
 	// our send and recieve strings
 	msgTx	= "";
 	msgRx	= "";
 
-	ofxTCPSettings settings("127.0.0.1", 11999);
-
-	// set other options:
-	//settings.blocking = false;
-	//settings.messageDelimiter = "\n";
-
-	// connect to the server - if this fails or disconnects
-	// we'll check every few seconds to see if the server exists
-	tcpClient.setup(settings);
-
-	// optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same
-	tcpClient.setMessageDelimiter("\n");
-
 	connectTime = 0;
 	deltaTime = 0;
+
+	//======================== JSON =============================
+	loadSettings();
+
+	//======================== GUI ==============================
+
+	
+	tcpGui.setup(); // most of the time you don't need a name
+	
+	//tcpGui.add(playBtn.setup(videoCommandsName[0]));
+	tcpGui.add(playBtn.setup(videoCommandsName[1]));
+	tcpGui.add(prevBtn.setup(videoCommandsName[2]));
+	tcpGui.add(nextBtn.setup(videoCommandsName[3]));
+	tcpGui.add(stopBtn.setup(videoCommandsName[6]));
+	tcpGui.add(volumeUp.setup(videoCommandsName[8]));
+	tcpGui.add(volumeDn.setup(videoCommandsName[9]));
+
+	tcpGui.setPosition(ofGetWidth() / 2, 10);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+
+	//======================== Network ==========================
 	if(tcpClient.isConnected()){
 		// we are connected - lets try to receive from the server
 		string str = tcpClient.receive();
@@ -41,8 +50,8 @@ void ofApp::update(){
 		deltaTime = ofGetElapsedTimeMillis() - connectTime;
 
 		if( deltaTime > 5000 ){
-			tcpClient.setup("127.0.0.1", 11999);
-			tcpClient.setMessageDelimiter("\n");
+			tcpClient.setup(ip, port);
+			tcpClient.setMessageDelimiter(delimiter);
 			connectTime = ofGetElapsedTimeMillis();
 		}
 
@@ -55,6 +64,8 @@ void ofApp::draw(){
 	ofSetColor(20);
 	ofDrawBitmapString("openFrameworks TCP Send Example", 15, 30);
 
+	//======================== Network ==========================
+
 	if(tcpClient.isConnected()){
 		if(!msgTx.empty()){
 			ofDrawBitmapString("sending:", 15, 55);
@@ -66,6 +77,11 @@ void ofApp::draw(){
 	}else{
 		ofDrawBitmapString("status: server not found. launch server app and check ports!\n\nreconnecting in "+ofToString( (5000 - deltaTime) / 1000 )+" seconds", 15, 55);
 	}
+
+	//======================== JSON =============================
+
+	//======================== GUI ==============================
+	tcpGui.draw();
 }
 
 
@@ -89,53 +105,61 @@ void ofApp::keyPressed(ofKeyEventArgs & key){
 	}
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-
+//======================== JSON =============================
+void ofApp::saveSettings() {
+	// now write pretty print
+	if (!settings.save("settings.json", true))
+	{
+		ofLogNotice("ofApp::setup") << "example_output_pretty.json written unsuccessfully.";
+	}
+	else
+	{
+		ofLogNotice("ofApp::setup") << "example_output_pretty.json written successfully.";
+	}
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::loadSettings() {
 
-}
+	std::string file = "settings.json";
+	// Now parse the JSON
+	bool parsingSuccessful = settings.open(file);
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+	if (parsingSuccessful)
+	{
+		ofLogNotice("ofApp::setup") << settings.getRawString();
 
-}
+		ip = settings["ip"].asString();
+		port = settings["port"].asDouble();
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+		ofxTCPSettings IPsettings(ip, port);
 
-}
+		// set other options:
+		//settings.blocking = false;
+		//settings.messageDelimiter = "\n";
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+		// connect to the server - if this fails or disconnects
+		// we'll check every few seconds to see if the server exists
+		tcpClient.setup(IPsettings);
 
-}
+		// optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same
+		delimiter = settings["message-delimiter"].asString();
+		tcpClient.setMessageDelimiter(delimiter);
+		
+		videoCommandsName.clear();
+		videoCommandsString.clear();
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+		ofLog() << " settings['video-commands'].size(); "<< settings["video-commands"].size();
 
-}
+		for (int i = 0; i < settings["video-commands"].size(); i++) {
+			videoCommandsName.push_back(settings["video-commands"][i]["name"].asString());
+			videoCommandsString.push_back(settings["video-commands"][i]["string"].asString());
+		}
+	
+	}
+	else
+	{
+		ofLogError("ofApp::setup") << "Failed to parse JSON" << endl;
+	}
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+	
 }
--- Test-networkTcpClientExample/src/ofApp.h
+++ Test-networkTcpClientExample/src/ofApp.h
@@ -2,6 +2,8 @@
 
 #include "ofMain.h"
 #include "ofxNetwork.h"
+#include "ofxJSON.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 
@@ -10,19 +12,20 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-		
 		void keyPressed(ofKeyEventArgs & key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);		
-		
+
+		void keyReleased(int key) {};
+		void mouseMoved(int x, int y) {};
+		void mouseDragged(int x, int y, int button) {};
+		void mousePressed(int x, int y, int button) {};
+		void mouseReleased(int x, int y, int button) {};
+		void mouseEntered(int x, int y) {};
+		void mouseExited(int x, int y) {};
+		void windowResized(int w, int h) {};
+		void dragEvent(ofDragInfo dragInfo) {};
+		void gotMessage(ofMessage msg) {};
+
+	//======================== Network ==========================
 		ofxTCPClient tcpClient;
 		string msgTx, msgRx;
 
@@ -33,6 +36,33 @@ class ofApp : public ofBaseApp{
 		int connectTime;
 		int deltaTime;
 
-	int size;
+		int size;
+
+		string ip;
+		int port;
+		string delimiter;
+
+		vector<string> videoCommandsName;
+		vector<string> videoCommandsString;
+
+	//======================== JSON =============================
+		ofxJSONElement settings;
+		void saveSettings();
+		void loadSettings();
+
+	//======================== Gui =========================
+		vector<ofxButton>	videoCommandBtn;
+		ofxButton playBtn;
+		ofxButton prevBtn;
+		ofxButton nextBtn;
+		ofxButton stopBtn;
+		ofxButton volumeVal;
+		ofxButton volumeUp;
+		ofxButton volumeDn;
+
+		ofxLabel status;
+
+		ofxPanel tcpGui;
+
 };
 
